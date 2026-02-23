01af73ca79dabb7b27669ebef67dd1555469bb18
lucianosaints <lucianosaints@gmail.com>
2026-02-23 06:50:42 -0300

    chore: configurações de produção para deploy na AWS EC2 (WhiteNoise, StaticRoot)

--- projeto_marica/settings.py
+++ projeto_marica/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',  # CORS Middleware deve vir antes do CommonMiddleware
     'django.middleware.common.CommonMiddleware',
@@ -129,6 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# Servir o frontend_simples via WhiteNoise
+WHITENOISE_ROOT = BASE_DIR / "frontend_simples"
 
 # Configuração de Media (Uploads de Arquivos e Imagens)
 import os
