35cfbca649733c2ff9636c3230d0408db4f0e273
lucianosaints <lucianosaints@gmail.com>
2026-02-26 23:02:17 -0300

    PWA: Habilita instalação do aplicativo no celular (Manifest, Service Worker, Ícones)

---
+++ frontend_simples/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'marica-cidadao-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo/Logo-prefeitura.png',
+  '/logo/Logo-Ictim.png',
+  '/logo/fundo.png',
+  '/logo/pwa_icon_512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
